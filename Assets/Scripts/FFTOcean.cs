48379cca198886ddfc28a25d584cf8c5ef23991d
baixue <52575446+baibaixue@users.noreply.github.com>
2022-02-26 17:42:59 +0800

    update FFT
    波效果实现
    尚存问题： 1. 法线 2. 频谱生成

--- Assets/Scripts/FFTOcean.cs
+++ Assets/Scripts/FFTOcean.cs
@@ -36,14 +36,16 @@ public class FFTOcean : MonoBehaviour
     private RenderTexture TempRT;               // 临时存储中间过程
     // 海洋参数
     public ComputeShader oceanCS;             // 计算海洋的compute shader
-    private float[] gaussianRandom;           // 高斯随机数
+    private float[] gaussianRandom;           // h0高斯随机数
+    private float[] gaussianRandomConj;       // h0Conj高斯随机数
     public Vector2 windDir;                   // 风向
     public float windSpeed;                   // 风速
     public float amplitude;                   // 菲利普参数，影响波浪高度
     public float timeScale;                   // 时间系数
 
 
-    private ComputeBuffer gaussBuff;            // 高斯随机数缓冲区
+    private ComputeBuffer gaussBuff;            // h0高斯随机数缓冲区
+    private ComputeBuffer gaussBuffConj;           // hoConj高斯随机数缓冲区
     private float time = 0;                     // 时间
 
     // KernelID
@@ -132,14 +134,22 @@ public class FFTOcean : MonoBehaviour
     {
         float pi = 3.1415926535f;
         gaussianRandom = new float[fftSize * fftSize * 2];
+        gaussianRandomConj = new float[fftSize * fftSize * 2];
         for (int i = 0; i < gaussianRandom.Length; i+=2)
         {
-            float u1 = Random.Range(0, 1.0f);
-            float u2 = Random.Range(0, 1.0f);
+            float u1 = Random.Range(0, 1f);
+            float u2 = Random.Range(0, 1f);
             float x1 = Mathf.Cos(2 * pi * u1) * Mathf.Sqrt(-2 * Mathf.Log(u2));
             float x2 = Mathf.Sin(2 * pi * u1) * Mathf.Sqrt(-2 * Mathf.Log(u2));
             gaussianRandom[i] = x1;
             gaussianRandom[i + 1] = x2;
+
+            float u3 = Random.Range(0, 1f);
+            float u4 = Random.Range(0, 1f);
+            float x3 = Mathf.Cos(2 * pi * u3) * Mathf.Sqrt(-2 * Mathf.Log(u4));
+            float x4 = Mathf.Sin(2 * pi * u3) * Mathf.Sqrt(-2 * Mathf.Log(u4));
+            gaussianRandomConj[i] = x3;
+            gaussianRandomConj[i + 1] = x4;
         }
 
         //DebugData();
@@ -179,8 +189,11 @@ public class FFTOcean : MonoBehaviour
         // 生成高斯随机数
         CreateGaussianRandom();
         gaussBuff = new ComputeBuffer(gaussianRandom.Length, 4);
+        gaussBuffConj = new ComputeBuffer(gaussianRandomConj.Length, 4);
         gaussBuff.SetData(gaussianRandom);
+        gaussBuffConj.SetData(gaussianRandomConj);
         oceanCS.SetBuffer(kernelCreateHeightSpectrum, "GaussianRandomList", gaussBuff);
+        oceanCS.SetBuffer(kernelCreateHeightSpectrum, "GaussianRandomListConj", gaussBuffConj);
     }
     // 每帧计算海洋数据
     private void CreateComputeShaderValue()
@@ -188,7 +201,7 @@ public class FFTOcean : MonoBehaviour
         windDir.Normalize();
         oceanCS.SetFloat("A", amplitude);
         oceanCS.SetFloat("time", time);
-        oceanCS.SetVector("windDir", windDir * windSpeed);
+        oceanCS.SetVector("windDir", new Vector2(windDir.x,windDir.y));
         oceanCS.SetFloat("windSpeed", windSpeed);
 
         // 生成高度频谱
@@ -204,14 +217,14 @@ public class FFTOcean : MonoBehaviour
         oceanCS.SetTexture(kernelCreateGradientSpectrum, "GradientXSpectrumRT", GradientXSpectrumRT);
         oceanCS.SetTexture(kernelCreateGradientSpectrum, "GradientZSpectrumRT", GradientZSpectrumRT);
         oceanCS.Dispatch(kernelCreateGradientSpectrum, fftSize / 8, fftSize / 8, 1);
-        /*
+
         for (int i = 1; i <= samplingPoint; i++)
         {
             // 横向向fft
             oceanCS.SetInt("isHV", 2);
             // 设置当前的阶段
             oceanCS.SetInt("stage", i);
-            int isFFTEnd = i == samplingPoint ? 1 : 0;
+            int isFFTEnd = i == samplingPoint ? -1 : 1;
             // 是否是FFT的最后一个阶段
             oceanCS.SetInt("isFFTEnd", isFFTEnd);
             
@@ -221,13 +234,14 @@ public class FFTOcean : MonoBehaviour
             ComputeFFT(kernelFFT, ref GradientXSpectrumRT);
             ComputeFFT(kernelFFT, ref GradientZSpectrumRT);
         }
-        for (int i = 1; i <= samplingPoint; i++)
+        
+        for (int j = 1; j <= samplingPoint ; j++)
         {
             // 纵向向fft
             oceanCS.SetInt("isHV", 1);
             // 设置当前的阶段
-            oceanCS.SetInt("stage", i);
-            int isFFTEnd = i == samplingPoint ? 1 : 0;
+            oceanCS.SetInt("stage", j);
+            int isFFTEnd = j == samplingPoint ? -1 : 1;
             // 是否是FFT的最后一个阶段
             oceanCS.SetInt("isFFTEnd", isFFTEnd);
 
@@ -238,23 +252,20 @@ public class FFTOcean : MonoBehaviour
             ComputeFFT(kernelFFT, ref GradientZSpectrumRT);
         }
         
-        
-        
         // 生成纹理
-        oceanCS.SetFloat("HeightScale", HeightScale);
-        oceanCS.SetFloat("DisplaceScale", DisplaceScale);
+       oceanCS.SetFloat("HeightScale", HeightScale);
+       oceanCS.SetFloat("DisplaceScale", DisplaceScale);
 
-        oceanCS.SetTexture(kernelCreateRenderTexture, "HeightSpectrumRT", HeightSpectrumRT);
-        oceanCS.SetTexture(kernelCreateRenderTexture, "DeviationXSpectrumRT", DeviationXSpectrumRT);
-        oceanCS.SetTexture(kernelCreateRenderTexture, "DeviationZSpectrumRT", DeviationZSpectrumRT);
-        oceanCS.SetTexture(kernelCreateRenderTexture, "GradientXSpectrumRT", GradientXSpectrumRT);
-        oceanCS.SetTexture(kernelCreateRenderTexture, "GradientZSpectrumRT", GradientZSpectrumRT);
-        oceanCS.SetTexture(kernelCreateRenderTexture, "DisplaceRT", DisplaceRT);
-        oceanCS.SetTexture(kernelCreateRenderTexture, "NormalRT", NormalRT);
-        oceanCS.Dispatch(kernelCreateRenderTexture, fftSize / 8, fftSize / 8, 1);
-        */
+       oceanCS.SetTexture(kernelCreateRenderTexture, "HeightSpectrumRT", HeightSpectrumRT);
+       oceanCS.SetTexture(kernelCreateRenderTexture, "DeviationXSpectrumRT", DeviationXSpectrumRT);
+       oceanCS.SetTexture(kernelCreateRenderTexture, "DeviationZSpectrumRT", DeviationZSpectrumRT);
+       oceanCS.SetTexture(kernelCreateRenderTexture, "GradientXSpectrumRT", GradientXSpectrumRT);
+       oceanCS.SetTexture(kernelCreateRenderTexture, "GradientZSpectrumRT", GradientZSpectrumRT);
+       oceanCS.SetTexture(kernelCreateRenderTexture, "DisplaceRT", DisplaceRT);
+       oceanCS.SetTexture(kernelCreateRenderTexture, "NormalRT", NormalRT);
+       oceanCS.Dispatch(kernelCreateRenderTexture, fftSize / 8, fftSize / 8, 1);
+       
         SetMaterialTexture();
-        
     }
     // 创建渲染纹理
     private RenderTexture CreateRenderTexture(int size)
@@ -269,9 +280,10 @@ public class FFTOcean : MonoBehaviour
     {
         oceanMaterial.SetTexture("_Displace", DisplaceRT);
         oceanMaterial.SetTexture("_Normal", NormalRT);
-        NormalMat.SetTexture("_MainTex", HeightSpectrumRT);
-        DisplaceMat.SetTexture("_MainTex", DeviationXSpectrumRT);
+        //NormalMat.SetTexture("_MainTex", HeightSpectrumRT);
+        DisplaceMat.SetTexture("_MainTex", DisplaceRT);
         //NormalMat.SetTexture("_MainTex", DeviationZSpectrumRT);
+        NormalMat.SetTexture("_MainTex", NormalRT);
     }
     private void ComputeFFT(int kernelID, ref RenderTexture inputRT)
     {
@@ -279,7 +291,9 @@ public class FFTOcean : MonoBehaviour
         oceanCS.SetTexture(kernelID, "OutputFFT", TempRT);
         oceanCS.Dispatch(kernelID, fftSize / 8, fftSize / 8, 1);
 
+        RenderTexture rt = inputRT;
         inputRT = TempRT;
+        TempRT = rt;
     }
     private void DebugData()
     {
@@ -301,5 +315,6 @@ public class FFTOcean : MonoBehaviour
     private void OnDisable()
     {
         gaussBuff.Dispose();
+        gaussBuffConj.Dispose();
     }
 }
